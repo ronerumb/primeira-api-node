ee7d0dd685af98f1e21859c9ffe2173a5356cc09
Roner <ronerumb@gmail.com>
2024-02-01 17:08:22 -0300

    finalizacao do client controller

--- src/modules/UserClient/controller/user-client-controller.ts
+++ src/modules/UserClient/controller/user-client-controller.ts
@@ -63,10 +63,94 @@ class UserClientController {
           });
         }
     }
-    public async listAll (req: Request, res:Response){}
-    public async update (req: Request, res:Response){}
-    public async delete (req: Request, res:Response){}
-}
+    public async listAll (req: Request, res:Response){
+
+      const tokenUserId = req.tokenUserId;
+      let page = Number(req.query.page);
+      if(!page || page <=0 || isNaN(page)){
+        page = 1;
+      }
+      try {
+        return res.json({
+          data : await userClientService.listAll(tokenUserId, page),
+        })
+        
+      } catch (err:any) {
+
+        return res.status(400).json({
+          message: err.message,
+        })
+        
+      }
+    }
+    public async update (req: Request, res:Response){
+      const { name, email, phone } = req.body;
+      const paramsId = req.params.id;
+      const tokenUserId = req.tokenUserId;
+  
+      try {
+        const ZClientShema = z.object({
+          name: z.string().min(1, { message: 'Nome é obrigatorio' }),
+          paramsId: z.string().min(30, { message: 'ID é obrigatorio' }),
+        });
+        ZClientShema.parse({ name, paramsId });
+      } catch (err: any) {
+        return res.status(400).json({
+          
+          error: err.errors,
+        });
+      }
+  
+      try {
+        return res.json({
+          message: 'Cliente atualizado',
+          data: await userClientService.update(
+            name,
+            email,
+            phone,
+            paramsId,
+            tokenUserId,
+          ),
+        });
+      } catch (err: any) {
+        return res.status(404).json({
+          error: err.message,
+        });
+      }
+          
+      
+    }
+    public async delete (req: Request, res:Response){
+
+      const paramsId = req.params.id;
+      const tokenUserId = req.tokenUserId;
+  
+      try {
+        const ZClientSchema = z
+          .string()
+          .min(30, { message: 'ID Obrigatorio' });
+        ZClientSchema.parse(paramsId);
+      } catch (err: any) {
+        return res.status(400).json({
+        
+          error: err.erros,
+        });
+      }
+  
+      try {
+        await userClientService.delete(paramsId, tokenUserId);
+        return res.json({
+          message: 'Deletado com sucesso',
+        });
+      } catch (err: any) {
+        return res.status(404).json({
+          error: err.message,
+        });
+      }
+    }
+
+    }
+
 
 
 export const userClientController = new UserClientController();
--- src/modules/UserClient/service/user-client-service.ts
+++ src/modules/UserClient/service/user-client-service.ts
@@ -39,9 +39,99 @@ class UserClientService {
 
 
     }
-    public async listAll() { }
-    public async update() { }
-    public async delete() { }
+    public async listAll(tokenUserId : string , page : number ){
+        const pageSize = 11;
+        const skip = (page - 1) * pageSize;
+
+        const findUser = await prismaConnect.user.findMany({
+            where : {
+                id: tokenUserId
+            },
+            include : {
+                userClient : {
+                    skip,
+                    take: pageSize
+                }
+            }
+        })
+
+        if (!findUser){
+            throw new Error('Não econtrado')
+        }
+
+        const totalCount = await prismaConnect.userClient.count({
+            where : {
+                userId : tokenUserId,
+            },
+        });
+
+        const totalPages = Math.ceil(totalCount / pageSize);
+
+        return {
+            page,
+            pageSize,
+            totalCount,
+            totalPages,
+            client : findUser[0].userClient,
+        }
+
+        
+     }
+    public async update( name: string,
+        email: string,
+        phone: number,
+        paramsId: string,
+        tokenUserId: string) { 
+
+            const findUserClient = await prismaConnect.userClient.findFirst({
+                where: {
+                  id: paramsId,
+                  userId: tokenUserId,
+                },
+              });
+          
+              if (!findUserClient) {
+                throw new Error('Não encontrado');
+              }
+          
+              const update = await prismaConnect.userClient.update({
+                where: {
+                  id: paramsId,
+                },
+                data: {
+                  name,
+                  email,
+                  phone,
+                },
+              });
+          
+              return update;
+        }
+        public async delete(paramsId: string, tokenUserId: string) {
+            const findUserClient = await prismaConnect.userClient.findFirst({
+              where: {
+                id: paramsId,
+                userId: tokenUserId,
+              },
+            });
+        
+            if (!findUserClient) {
+              throw new Error('Não encontrado');
+            }
+        
+            const deletUserClient = await prismaConnect.userClient.delete({
+              where: {
+                id: paramsId,
+              },
+            });
+        
+            UtilsFileUser.deleteFolderUser([
+              deletUserClient.userId,
+              deletUserClient.id,
+            ]);
+        
+            return deletUserClient;
+          }
 }
 
 
